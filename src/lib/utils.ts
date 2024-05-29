f55bd7bc0cde65d2f003ecf53f541341ddae7eb0
partyparrotgreg <2233348+partyparrotgreg@users.noreply.github.com>
2024-05-29 16:04:59 +0100

    testing env

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -7,5 +7,5 @@ export function cn(...inputs: ClassValue[]) {
 
 export const apiEndpointURL =
   process.env.NODE_ENV === "development"
-    ? "http://localhost:3000/api"
-    : `https://${process.env.VERCEL_URL}/api`;
+    ? "http://localhost:3000"
+    : `https://${process.env.VERCEL_URL}`;
